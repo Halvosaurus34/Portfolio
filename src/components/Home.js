b3dd2a9f949ddba4c45aff8f6f05ef6b268a54df
Levi Halvorson <lhalvorson34@gmail.com>
2020-11-26 08:39:25 -0700

    changed resume and brand statement

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,16 +12,7 @@ export default function Home(){
                 className="w-25 rounded-circle border border-black float-left m-2"
                 />
                 <div className="lead p-2">
-                I am a full stack development student, leveraging 7+ years of
-                transferable skills, adaptability, and teamwork. Past professional
-                experiences that overlapped with development work sparked a new passion
-                for the profession. Recently enrolled in a certificate program in full
-                stack development through an intensive 12-week bootcamp from the
-                University of Toronto. Described as friendly, well-mannered, and
-                detail-oriented. Whose background fostered skills in management,
-                troubleshooting, customer service, digital, and business analysis.
-                Excellent communicator with loyal and cooperative interactions with all
-                levels of management, clients, and vendors.
+                Full Stack Web Developer leveraging my background in the oil sector to build more intuitive user experiences on the web. Recently earned a certificate in Full Stack Development from the University of Toronto and developed skills in JavaScript, CSS, MERN stack, and responsive web design. Known as an innovative problem-solver passionate about developing apps, with a focus on mobile-first design and development. Aim for each project is to best engage the audience for an impactful user-experience.   Excited to leverage my skills in the future as part of a fast-paced, quality-driven team in order to build better experiences on the web.
                 </div>
         </div>
     
