63e786ef255bdb9fffb351dc157568062c7a897b
Levi <lhalvorson34@gmail.com>
2021-03-08 13:06:51 -0700

    changed colors

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./App.css";
 import NavBar from "../components/NavBar";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "../components/Home";
+import Contact from "../components/Contact";
+import Portfolio from "../components/Portfolio";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar />
+        <Router basename={process.env.PUBLIC_URL}>
+          <NavBar />
+
+          <Switch>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/Portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
