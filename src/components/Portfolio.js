16d4662855c1fd1871f041fa88a7ef4260288125
Levi <lhalvorson34@gmail.com>
2021-03-30 22:01:50 -0600

    added scribble to portf

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ import dayPlanner from "../assets/dayPlannerIcon.png";
 import stock from "../assets/stockIcon.png";
 import fitness from "../assets/fitness.png";
 import wetBat from "../assets/wetBatIcon2.PNG";
-
+import editor from "../assets/editor.png";
 export default function Portfolio() {
   return (
     <div
@@ -14,6 +14,51 @@ export default function Portfolio() {
     >
       <div className="display-4 ml-5">My Portfolio</div>
       <div className="row m-3 justify-content-center">
+        <div className="card p-2 col-lg-4 col-md-6 col-sm-12 mb-2">
+          <img
+            src={wetBat}
+            className="card-img-top"
+            alt="..."
+            style={{ height: "20rem" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Wet-Bat Challenge</h5>
+            <p className="card-text">
+              A coding challenge to make a travel website where you can create
+              travel quotes and view a list of quotes.
+            </p>
+            <br />
+            <a
+              href="https://shielded-badlands-63088.herokuapp.com/"
+              className="btn btn-dark mr-1"
+            >
+              Website
+            </a>
+            <a
+              href="https://github.com/Halvosaurus34/wet-bat-challenge"
+              className="btn btn-dark"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+
+        <div className="card p-2 col-lg-4 col-md-6 col-sm-12 mb-2">
+          <img src={editor} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">JS-Scribblify</h5>
+            <p className="card-text">
+              An interactive coding environment I published to npm!
+            </p>
+            <br />
+            <a
+              href="https://github.com/Halvosaurus34/jbook"
+              className="btn btn-dark"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
         <div className="card p-2 col-lg-4 col-md-6 col-sm-12 mb-2">
           <img src={weather} className="card-img-top" alt="..." />
           <div className="card-body">
@@ -30,29 +75,6 @@ export default function Portfolio() {
             </a>
             <a
               href="https://github.com/Halvosaurus34/Weather-App"
-              className="btn btn-dark"
-            >
-              GitHub
-            </a>
-          </div>
-        </div>
-        <div className="card p-2 col-lg-4 col-md-6 col-sm-12 mb-2">
-          <img src={quiz} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Code Quiz</h5>
-            <p className="card-text">
-              A timed quiz about JavaScript! Test your skills and try to get the
-              highscore!
-            </p>
-            <br />
-            <a
-              href="https://halvosaurus34.github.io/Code-Quiz/index.html"
-              className="btn btn-dark mr-1"
-            >
-              Website
-            </a>
-            <a
-              href="https://github.com/Halvosaurus34/Code-Quiz"
               className="btn btn-dark"
             >
               GitHub
@@ -120,34 +142,6 @@ export default function Portfolio() {
             </a>
             <a
               href="https://github.com/Halvosaurus34/Fitness-Tracker"
-              className="btn btn-dark"
-            >
-              GitHub
-            </a>
-          </div>
-        </div>
-        <div className="card p-2 col-lg-4 col-md-6 col-sm-12 mb-2">
-          <img
-            src={wetBat}
-            className="card-img-top"
-            alt="..."
-            style={{ height: "20rem" }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">Wet-Bat Challenge</h5>
-            <p className="card-text">
-              A coding challenge to make a travel website where you can create
-              travel quotes and view a list of quotes.
-            </p>
-            <br />
-            <a
-              href="https://shielded-badlands-63088.herokuapp.com/"
-              className="btn btn-dark mr-1"
-            >
-              Website
-            </a>
-            <a
-              href="https://github.com/Halvosaurus34/wet-bat-challenge"
               className="btn btn-dark"
             >
               GitHub
